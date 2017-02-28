c6807df85669fca1e475b236de200f9002e0f9d9
Live session user <richdev135@gmail.com>
2017-02-28 00:19:16 +0000

    add event handlers to show text

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,22 @@
-$("#more-king-grass").on("click", function(){
-    alert("click");
+
+$("#more-king-grass").bind("click", function(){
+    // hide more...
+    $(this).hide();
+    //show text
+    $("#less-king-grass").show(200);
+});
+
+
+$("#more-more-viaspace").bind("click", function(){
+    // hide more...
+    $(this).hide();
+    //show text
+    $("#less-more-viaspace").show(200);
+});
+
+$("#more-labs").bind("click", function(){
+    // hide more...
+    $(this).hide();
+    //show text
+    $("#less-labs").show(200);
 });
