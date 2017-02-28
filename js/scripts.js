71364f8ef7363654abf7fac3020559f93933ccd5
Live session user <richdev135@gmail.com>
2017-02-28 00:26:43 +0000

    corrections to handlers

--- js/scripts.js
+++ js/scripts.js
@@ -7,11 +7,11 @@ $("#more-king-grass").bind("click", function(){
 });
 
 
-$("#more-more-viaspace").bind("click", function(){
+$("#more-viaspace").bind("click", function(){
     // hide more...
     $(this).hide();
     //show text
-    $("#less-more-viaspace").show(200);
+    $("#less-viaspace").show(200);
 });
 
 $("#more-labs").bind("click", function(){
